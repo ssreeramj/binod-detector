c383f679929fc191c37270780239a9dc676d6305
ssreeramj <ssreeramj@gmail.com>
2020-08-09 21:21:29 +0530

    added wordcloud plot functionality

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import json
 import os
+import pandas as pd 
+import texthero as hero
 
 from dotenv import load_dotenv
 from googleapiclient.discovery import build
@@ -7,7 +9,7 @@ from googleapiclient.discovery import build
 load_dotenv()
 
 API_KEY = os.getenv('YOUTUBE_API_KEY')
-VIDEO_ID = 'sW12dNPz_7c'
+VIDEO_ID = 'CbNYxFIQ5jI'
 
 # get list of comments from a response object
 def get_comments(response):
@@ -19,6 +21,8 @@ def get_comments(response):
 
     return comments
 
+# getting comments from api
+count = 10
 all_comments = []
 youtube = build('youtube', 'v3', developerKey=API_KEY)
 
@@ -33,7 +37,7 @@ all_comments.extend(get_comments(response))
 
 next_page_token = response.get('nextPageToken', 0)
 
-while next_page_token:
+while next_page_token and count:
     next_request = youtube.commentThreads().list_next(
         request, response
     )
@@ -43,7 +47,17 @@ while next_page_token:
 
     next_page_token = next_response.get('nextPageToken', 0)
     request, response = next_request, next_response
-
+    count -= 1
 
 with open('comments.txt', 'w') as f:
     json.dump(all_comments, f, indent=2)
+
+# data preprocessing
+# df = pd.DataFrame(all_comments, columns=['text'])
+# df['clean_text'] = hero.clean(df['text'])
+
+# # print(df.head())
+# figure = hero.visualization.wordcloud(df['clean_text'], return_figure=True)
+# figure.savefig('plot.png')
+
+
