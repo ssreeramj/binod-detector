cfdf632699ba23e0b71b0488c76cbe539aa2c08c
ssreeramj <ssreeramj@gmail.com>
2020-08-10 00:50:32 +0530

    completed the streamlit app

---
+++ app.py
@@ -0,0 +1,57 @@
+import os
+import re
+import streamlit as st
+
+from main import get_all_comments, get_wordcloud, get_pie_plot
+
+from dotenv import load_dotenv
+from googleapiclient.discovery import build
+
+load_dotenv()
+
+API_KEY = os.getenv('YOUTUBE_API_KEY')
+
+youtube = build('youtube', 'v3', developerKey=API_KEY)
+
+if __name__ == '__main__':
+    st.title('Binod Detector:rocket:')
+
+    st.header("Unless you are living under a rock, you must have come across \
+        the word 'BINOD' on social media. This simple webapp helps you visualize \
+        comment section of a youtube video and gives us the percentage of comments \
+        having the word in it.")
+
+    url = st.text_input(label='Enter the url of a youtube video.')
+    pattern = re.compile('^(http(s)?:\/\/)?((w){3}.)?youtu(be|.be)?(\.com)?\/.+')
+
+    if re.match(pattern, url):
+        try:
+            video_id = url.split('?')[1].split('=')[1]
+            if '&' in video_id:
+                video_id = video_id.split('&')[0]
+
+            with st.spinner('Scraping comments...'):
+                comments_list = get_all_comments(youtube, video_id)
+
+            st.balloons()
+            word_cloud, df = get_wordcloud(comments_list)
+
+            pie_plot = get_pie_plot(df)
+
+            st.subheader('Lets have a look at some random comments of the youtube video')
+            st.dataframe(df.loc[:, 'text'].sample(20))
+
+            st.subheader('Here we can see the visualization of common words that \
+                are there in the comments.')
+            st.write(word_cloud)
+
+            st.subheader("Now we see the distribution of comments which have 'BINOD' \
+                in them.")
+            st.write(pie_plot)
+        except Exception as e:
+            # st.subheader(e)
+            st.subheader('Could not get the video id, please paste the correct URL')
+
+    else:
+        st.subheader('Please enter a valid URL of a youtube video')
+
--- main.py
+++ main.py
@@ -1,15 +1,9 @@
 import json
 import os
 import pandas as pd 
+import plotly.express as px
 import texthero as hero
 
-from dotenv import load_dotenv
-from googleapiclient.discovery import build
-
-load_dotenv()
-
-API_KEY = os.getenv('YOUTUBE_API_KEY')
-VIDEO_ID = 'CbNYxFIQ5jI'
 
 # get list of comments from a response object
 def get_comments(response):
@@ -21,43 +15,68 @@ def get_comments(response):
 
     return comments
 
-# getting comments from api
-count = 10
-all_comments = []
-youtube = build('youtube', 'v3', developerKey=API_KEY)
+# get all comments
+def get_all_comments(youtube, video_id, count=20):
+    all_comments = []
 
-request = youtube.commentThreads().list(
-    part='snippet',
-    videoId=VIDEO_ID,
-    maxResults=100,
-    textFormat='plainText',
-)
-response = request.execute()
-all_comments.extend(get_comments(response))
-
-next_page_token = response.get('nextPageToken', 0)
-
-while next_page_token and count:
-    next_request = youtube.commentThreads().list_next(
-        request, response
+    request = youtube.commentThreads().list(
+        part='snippet',
+        videoId=video_id,
+        maxResults=100,
+        textFormat='plainText',
     )
+    response = request.execute()
+    all_comments.extend(get_comments(response))
 
-    next_response = next_request.execute()
-    all_comments.extend(get_comments(next_response))
+    next_page_token = response.get('nextPageToken', 0)
 
-    next_page_token = next_response.get('nextPageToken', 0)
-    request, response = next_request, next_response
-    count -= 1
+    while next_page_token and count:
+        next_request = youtube.commentThreads().list_next(
+            request, response
+        )
 
-with open('comments.txt', 'w') as f:
-    json.dump(all_comments, f, indent=2)
+        next_response = next_request.execute()
+        all_comments.extend(get_comments(next_response))
+
+        next_page_token = next_response.get('nextPageToken', 0)
+        request, response = next_request, next_response
+        count -= 1
+
+    return all_comments
+
+# with open('comments.txt', 'w') as f:
+#     json.dump(all_comments, f, indent=2)
 
 # data preprocessing
-# df = pd.DataFrame(all_comments, columns=['text'])
-# df['clean_text'] = hero.clean(df['text'])
+def get_wordcloud(all_comments):
+    df = pd.DataFrame(all_comments, columns=['text'])
+    df['clean_text'] = hero.clean(df['text'])
 
-# # print(df.head())
-# figure = hero.visualization.wordcloud(df['clean_text'], return_figure=True)
-# figure.savefig('plot.png')
+    figure = hero.visualization.wordcloud(df['clean_text'], return_figure=True)
+    # figure.savefig('plot.png')
+    return figure, df
+
+
+def get_pie_plot(df):
+    top_words = hero.visualization.top_words(df['clean_text'])
+
+    df['is_binod'] = df['clean_text'].apply(lambda x: 'binod' in x)
+
+    values = list(df['is_binod'].value_counts())
+
+    if len(values) != 2:
+        most_common_word = hero.visualization.top_words(df['clean_text']).keys()[0]
+        df['is_common_word'] = df['clean_text'].apply(lambda x: most_common_word in x)
+
+        values = list(df['is_common_word'].value_counts())
+        names = ['Others', str.capitalize(most_common_word)]
+    else:
+        names = ['Others', 'Binod']
+
+    pie_fig = px.pie(values=values, names=names)
+
+    return pie_fig
+
+
 
 
